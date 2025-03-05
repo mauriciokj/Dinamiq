9f7554d54deabb52faf6808d8eff0511a3c70a4f
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2025-03-05 16:11:43 -0300

    so ta computando um voto

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,13 @@
 # app/controllers/votes_controller.rb
 class VotesController < ApplicationController
   def create
+    logger.info "Parâmetros recebidos: #{params.inspect}"
     option = Option.find(params[:option_id])
-    option.update!(votes: option.votes || 0 + 1)
-
+    logger.info "Option antes do update: #{option.inspect}"
+    
+    option.update!(votes: (option.votes || 0) + 1)
+    logger.info "Option depois do update: #{option.reload.inspect}"
+    
     # Broadcast via Action Cable
     ActionCable.server.broadcast("poll_#{params[:token]}", option.poll.options)
 
