ba31342d77f44c33f47baa677a61e6e7f3b2ca28
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2025-03-17 15:33:18 -0300

    corrigido cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5174", "localhost:5173", "127.0.0.1:5173", 'http://72.167.132.22:3009' # Porta do React
+    origins "localhost:5174", "localhost:5173", "127.0.0.1:5173",
+    "http://72.167.132.22:3009", "populiz.com", "www.populiz.com",
+    "https://populiz.com", "https://www.populiz.com"
 
     resource "*",
       headers: :any,
