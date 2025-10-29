84a40cb3cdd0964bf89e2cee89fa2835c39ed569
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2025-10-29 16:04:53 -0300

    melhorias no processo de portas do server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,12 +60,11 @@ Rails.application.configure do
   config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.allowed_request_origins = [
-  "http://localhost:3000", # React (se estiver em porta diferente)
-  "http://localhost:5173",  # Vite/Outros
-  "http://localhost:5174",  # Vite/Outros
-]
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = ['*']
+
+  # Permitir conexões de qualquer host em desenvolvimento
+  config.hosts.clear
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
